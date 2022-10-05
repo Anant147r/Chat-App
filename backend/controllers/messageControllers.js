df193a22be0c6b4122068a983f1b7bdf4807fe69
Anant Rawat <anant.rawat@sunfox.in>
2022-10-05 17:03:48 +0530

    Api created for messages

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 const Message = require("../models/messageModel");
+const Chat = require("../models/chatModel");
 
 const sendMessage = asyncHandler(async (req, res) => {
   const { content, chatId } = req.body;
@@ -15,9 +17,31 @@ const sendMessage = asyncHandler(async (req, res) => {
 
   try {
     var message = await Message.create(newMessage);
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
-  } catch (err) {}
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+    res.json(message);
+  } catch (err) {
+    res.status(400);
+    throw new Error(err.message);
+  }
 });
 
-module.exports = { sendMessage };
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (err) {
+    res.status(400);
+    throw new Error(err.message);
+  }
+});
+
+module.exports = { sendMessage, allMessages };
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers");
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage);
-// router.route("/:chatId").get(protect,allMessages);
+router.route("/:chatId").get(protect, allMessages);
 module.exports = router;
